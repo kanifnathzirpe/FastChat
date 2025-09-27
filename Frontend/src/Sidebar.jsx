84f0aaf9d1d00453c11f285d3e1a1096149ecf3b
Kanifnath Zirpe <kanifnathzirpe1@gmail.com>
2025-09-27 19:10:11 +0530

    Update Sidebar.jsx

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -8,7 +8,7 @@ function Sideabar() {
     const {allThreads, setAllThreads, currThreadId, newChat, setNewChat, setPrompt, setReply, setcurrThreadId, setPrevChats} = useContext(MyContext);
     const getAllThreads = async () => {
         try {
-            const response = await fetch("http://localhost:8080/api/thread");
+            const response = await fetch("https://fast-chat-backend.onrender.com/api/thread");
             const res = await response.json();
             const filteredData = res.map(thread => ({threadId: thread.threadId, title: thread.title}));
             setAllThreads(filteredData);
@@ -33,7 +33,7 @@ function Sideabar() {
         setNewChat(false);
 
         try {
-            const response = await fetch(`http://localhost:8080/api/thread/${newThreadId}`);
+            const response = await fetch(`https://fast-chat-backend.onrender.com/api/thread/${newThreadId}`);
             const res = await response.json();
             setPrevChats(res);
             setNewChat(false);
@@ -46,7 +46,7 @@ function Sideabar() {
     }
     const deleteThread = async (threadId) => {
         try {
-            const response = await fetch(`http://localhost:8080/api/thread/${threadId}`, {method: "DELETE"});
+            const response = await fetch(`https://fast-chat-backend.onrender.com/api/thread/${threadId}`, {method: "DELETE"});
             const res = await response.json();
             console.log(res);
             setAllThreads(allThreads.filter(thread => thread.threadId !== threadId));
